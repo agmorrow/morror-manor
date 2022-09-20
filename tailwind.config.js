df387fa4378f55c5a0c88f27937a215f321c4322
austinmorrow <morrow.austin5@gmail.com>
2022-09-19 21:42:45 -0600

    work in progress about

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,14 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
   ],
 }
