32b98a08770669374d1659664f7c63b2f9ba7e59
austinmorrow <morrow.austin5@gmail.com>
2022-09-26 13:08:49 -0600

    fixed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,8 @@
 <template>
   <footer class="text-center lg:text-left bg-gray-200 text-gray-600">
 
-    <div class="mx-6 py-10 text-center md:text-center">
-      <div class="">
-        <p class="flex items-center justify-center md:justify-start">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone" class="w-4 mr-4" role="img"
-            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path fill="currentColor"
-              d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z">
-            </path>
-          </svg>
-          720-985-9184
-        </p>
-        <p class="flex items-center justify-center md:justify-start">
+    <div class="mx-6 pt-4 text-center md:flex justify-around">
+      <p class="flex items-center justify-center md:justify-start">
           <a href="https://www.instagram.com/morrowmanorphoto/" class="mr-4 text-gray-600">
             <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" class="w-3.5" role="img"
               xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
@@ -23,6 +13,15 @@
           </a>
           @morrowmanorphoto
         </p>
+      <p class="flex items-center justify-center md:justify-start">
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone" class="w-4 mr-4" role="img"
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path fill="currentColor"
+              d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z">
+            </path>
+          </svg>
+          720-985-9184
+        </p>
         <p class="flex items-center justify-center md:justify-start">
           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" class="w-4 mr-4" role="img"
             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
@@ -32,9 +31,9 @@
           </svg>
           contact@morrowmanorphoto.com
         </p>
-      </div>
+      
     </div>
-    <div class="text-center p-6 bg-gray-200">
+    <div class="text-center p-4 bg-gray-200">
       © 2022 Copyright Morrow Manor Photography </div>
   </footer>
 </template>
