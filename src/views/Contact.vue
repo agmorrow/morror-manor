dd83f48867bd23e152cbf44b73ab2c29075f511a
austinmorrow <morrow.austin5@gmail.com>
2022-09-29 16:47:01 -0600

    fixed footer and contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,10 @@
 <template>
 
-<!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.min.js"></script>
-<div style="width:100%; min-height:500px">
-<iframe src="https://hello.hecticapp.com/01/morrow-manor-photography/new-form?inFrame=true" id="hectic-form" style="padding: 0px; margin: 0px; border: 0; max-width: 100%; min-width: 100%"></iframe>
+<!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.min.js"></script> -->
+<div class="max-w-full">
+<iframe src="https://hello.hecticapp.com/01/morrow-manor-photography/new-form?inFrame=true" id="hectic-form" class="max-w-full min-w-full h-full"></iframe>
 </div>
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 setTimeout(() => iFrameResize({heightCalculationMethod: 'min', sizeWidth: true, sizeHeight: true, log: false, checkOrigin: false}, '#hectic-form'),100);
 </script> -->
 
