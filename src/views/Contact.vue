a07b7fcfbbdfc661f4373f5d7db59f8b973009c4
Austin Morrow <morrow.austin5@gmail.com>
2022-12-05 20:40:30 -0700

    Leave contact alone for now

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,12 +1,3 @@
 <template>
 
-<!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.min.js"></script> -->
-<div class="max-w-full">
-<iframe src="https://hello.hecticapp.com/01/morrow-manor-photography/new-form?inFrame=true" id="hectic-form" class="max-w-full min-w-full h-full"></iframe>
-</div>
-<!-- <script type="text/javascript">
-setTimeout(() => iFrameResize({heightCalculationMethod: 'min', sizeWidth: true, sizeHeight: true, log: false, checkOrigin: false}, '#hectic-form'),100);
-</script> -->
-
-
 </template>
